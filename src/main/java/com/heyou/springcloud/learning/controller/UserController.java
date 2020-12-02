d358b1305db7ac4fdba09c4daf49df737f3c1fab
heyou-macbook <heyou_0423@163.com>
2020-12-02 19:21:08 +0800

    add:控制器实现

---
+++ src/main/java/com/heyou/springcloud/learning/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.heyou.springcloud.learning.controller;
+
+import com.heyou.springcloud.learning.dao.UserRepository;
+import com.heyou.springcloud.learning.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+/**
+ * @author heyou(heyou_0423 @ 163.com)
+ * @date 2020/11/30 23:28
+ */
+@RequestMapping("/users")
+@RestController
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/{id}")
+    public Optional<User> findById(@PathVariable Long id) {
+        return this.userRepository.findById(id);
+    }
+}
