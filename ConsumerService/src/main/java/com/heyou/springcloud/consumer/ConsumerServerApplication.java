0c64e0b5386c80f843a34cde57030a23f5895dc2
heyou-macbook <heyou_0423@163.com>
2020-12-02 23:24:25 +0800

    add:消费者服务

---
+++ ConsumerService/src/main/java/com/heyou/springcloud/consumer/ConsumerServerApplication.java
@@ -0,0 +1,16 @@
+package com.heyou.springcloud.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author heyou(heyou_0423 @ 163.com)
+ * @date 2020/12/2 23:16
+ */
+@SpringBootApplication
+public class ConsumerServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerServerApplication.class, args);
+    }
+
+}
