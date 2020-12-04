2cc4ac595b5bf1e769cf85281426fe6b5c0dfbc9
heyou-macbook <heyou_0423@163.com>
2020-12-04 23:01:36 +0800

    update:修改服务名称

--- microservice-consumer-movie-ribbon/src/main/java/com/heyou/springcloud/consumer/ribbon/controller/MovieController.java
+++ microservice-consumer-movie-ribbon/src/main/java/com/heyou/springcloud/consumer/ribbon/controller/MovieController.java
@@ -1,6 +1,6 @@
 package com.heyou.springcloud.consumer.ribbon.controller;
 
-import com.heyou.springcloud.consumer.entity.User;
+import com.heyou.springcloud.consumer.ribbon.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +21,11 @@ public class MovieController {
     @GetMapping("/users/{id}")
     public User findById(@PathVariable Long id) {
         // 这里用到了RestTemplate的占位符能力
-        User user = this.restTemplate.getForObject("http://localhost:8011/users/{id}", User.class, id);
+        User user = this.restTemplate.getForObject(
+                "http://microservice-provider-user/users/{id}",
+                User.class,
+                id
+        );
         // ...电影微服务的业务...
         return user;
     }
