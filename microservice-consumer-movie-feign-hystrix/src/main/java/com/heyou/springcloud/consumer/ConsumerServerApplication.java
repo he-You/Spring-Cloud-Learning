9849c288e20f8f8297e68f7443e39869796715b6
heyou-macbook <heyou_0423@163.com>
2020-12-12 22:49:04 +0800

    add:使用 Feign 中的 Hystrix

---
+++ microservice-consumer-movie-feign-hystrix/src/main/java/com/heyou/springcloud/consumer/ConsumerServerApplication.java
@@ -0,0 +1,24 @@
+package com.heyou.springcloud.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author heyou(heyou_0423 @ 163.com)
+ * @date 2020/12/2 23:16
+ */
+@SpringBootApplication
+@EnableFeignClients
+public class ConsumerServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerServerApplication.class, args);
+    }
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
