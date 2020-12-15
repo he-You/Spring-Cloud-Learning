45bfe202e8c4a9c8059845e730109796a7a0d125
heyou-macbook <heyou_0423@163.com>
2020-12-15 23:27:08 +0800

    add:开启Hystrix-dashboard

---
+++ microservice-hystrix-dashboard/src/main/java/com/heyou/springcloud/consumer/HystrixDashboardApplication.java
@@ -0,0 +1,17 @@
+package com.heyou.springcloud.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author heyou(heyou_0423 @ 163.com)
+ * @date 2020/12/2 23:16
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardApplication.class, args);
+    }
+}
