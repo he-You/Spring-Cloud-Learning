05cac7c1d03197ebba56ec132301448965bfbbe3
heyou-macbook <heyou_0423@163.com>
2020-12-20 22:42:53 +0800

    add:Zuul回退

---
+++ microservice-gateway-zuul-fallback/src/main/java/com/heyou/springcloud/zuul/ZuulFallbackApplication.java
@@ -0,0 +1,19 @@
+package com.heyou.springcloud.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author heyou(heyou_0423 @ 163.com)
+ * @date 2020/12/2 22:57
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulFallbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulFallbackApplication.class, args);
+    }
+
+}
