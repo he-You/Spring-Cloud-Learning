3607f561aad69a39ea44283fe58f7303e252d3f9
heyou-macbook <heyou_0423@163.com>
2020-12-23 23:49:39 +0800

    add:ConfigServer 配置服务

---
+++ microservice-config-server/src/main/java/com/heyou/springcloud/configserver/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package com.heyou.springcloud.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author heyou(heyou_0423 @ 163.com)
+ * @date 2020/12/2 22:57
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
